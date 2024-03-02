21bb05f5a7eff38a737918ad3279661c8b7c21f9
FawazKhan1011 <138883345+FawazKhan1011@users.noreply.github.com>
2024-03-02 16:52:14 +0530

    CategoryPage

--- client/pronavigator/src/App.js
+++ client/pronavigator/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import './components/loginPage.js'
+import CategorySelectionPage from './components/CategorySelectionPage.js';
 function App() {
   return (
     <>
-    <loginPage/>
+    <CategorySelectionPage/>
     </>
   );
 }
-
 export default App;
+
