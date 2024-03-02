f885e1037b550ffdb945157432ec8f77288a1013
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-03-02 16:12:18 +0530

    Navbar Added

--- client/pronavigator/src/App.js
+++ client/pronavigator/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Login from './components/loginPage';
 import Register from './components/registerPage';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <>
     {/* <Login></Login> */}
-    <Register/>
+    {/* <Register/> */}
+    <Navbar/>
     </>
   );
 }
