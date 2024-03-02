5e84906692d692f63e2edb8c4df68a3cb2913918
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-03-03 01:16:52 +0530

    Github Repo Data fetched

--- client/pronavigator/src/App.js
+++ client/pronavigator/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import Login from './components/loginPage';
 import Register from './components/registerPage';
-
+import GitHubRepositories from './components/githubRepo';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/githubRepos' element={<GitHubRepositories/>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
