0f2639c24f678f486f33324e63ec15c1c89af7ad
FawazKhan1011 <138883345+FawazKhan1011@users.noreply.github.com>
2024-03-03 01:26:39 +0530

    Home Page

--- client/pronavigator/src/components/homePage.js
+++ client/pronavigator/src/components/homePage.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import '../styles/HomePage.css';
 import Navbar from './Navbar';
-import ProjectCard from './ProjectCard';
+import ProjectCard from './ProjectCard.js';
 
 function HomePage() {
   return (
@@ -10,25 +10,34 @@ function HomePage() {
       <Navbar />
       <div className="Home-page">
         <header className="welcome-section">
-          <h1 id='title'>WELCOME BACK</h1>
+          <h1 id='title'>WELCOME BACK,</h1>
+          <p id='subtitle'>Fawaz Khan</p>
         </header>
-        <div class = "gap"></div>
-        <div class = "cards">
+        <div className="gap"></div>
+        <div className="cards">
+          <section className="projects-section">
+            <h2 className="heading">Recommended Projects</h2>
+            <div className="projects-scroll">
+              <ProjectCard title="Project 1" />
+              <ProjectCard title="Project 2" />
+              <ProjectCard title="Project 3" />
+              <ProjectCard title="Project 4" />
+              <ProjectCard title="Project 5" />
+              <ProjectCard title="Project 6" />
+              <ProjectCard title="Project 7" />
+              {/* Add more ProjectCards as needed */}
+            </div>
+          </section>
         <section className="projects-section">
-          <h2>Recommended Projects</h2>
+          <h2 class = "heading">Recent Projects</h2>
           <div className="projects-scroll">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
-            {/* Add more ProjectCards as needed */}
-          </div>
-        </section>
-        <section className="projects-section">
-          <h2>Recent Projects</h2>
-          <div className="projects-scroll">
-            <ProjectCard />
-            <ProjectCard />
-            <ProjectCard />
+          <ProjectCard title="Project 1" />
+              <ProjectCard title="Project 2" />
+              <ProjectCard title="Project 3" />
+              <ProjectCard title="Project 4" />
+              <ProjectCard title="Project 5" />
+              <ProjectCard title="Project 6" />
+              <ProjectCard title="Project 7" />
             {/* Add more ProjectCards as needed */}
           </div>
         </section>
