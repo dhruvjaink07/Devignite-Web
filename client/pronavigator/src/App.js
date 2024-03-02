cf1a00a3cd6e545d63ee3c1699770740e1e2ad9d
FawazKhan1011 <138883345+FawazKhan1011@users.noreply.github.com>
2024-03-03 01:32:46 +0530

    changes

--- client/pronavigator/src/App.js
+++ client/pronavigator/src/App.js
@@ -6,12 +6,15 @@ import Login from './components/loginPage';
 import Register from './components/registerPage';
 import GitHubRepositories from './components/githubRepo';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import HomePage from './components/HomePage';
 function App() {
   return (
     <>
     <BrowserRouter>
       <Routes>
+        <Route path='/categorySelectionPage' element={<CategorySelectionPage/>}></Route>
         <Route path='/githubRepos' element={<GitHubRepositories/>}></Route>
+        <Route path='/home' element={<HomePage/>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
