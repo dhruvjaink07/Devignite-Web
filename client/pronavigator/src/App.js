05a96ac9dad03ebcc4f40f4f8220c8cec1282a4d
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2024-03-03 05:39:14 +0530

    Karle bhai

--- client/pronavigator/src/App.js
+++ client/pronavigator/src/App.js
@@ -1,5 +1,6 @@
 // src/App.js
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import { useCookies } from 'react-cookie';
 import './App.css';
 import CategorySelectionPage from './components/CategorySelectionPage';
 import Login from './components/loginPage';
@@ -9,34 +10,34 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import RepoDetails from './components/RepoDetails';
 function App() {
-  const [cookie,setCookie] = useCookies(['token'])
-  const [user,setUser] = useState('')
+  // const [cookie,setCookie] = useCookies(['token'])
+  // const [user,setUser] = useState('')
 
-  useEffect(()=>{
-    const getUser = () =>{
-      fetch('http://127.0.0.1:3001/api/auth/me',{
-        method:'GET',
-        headers:{
-          'Content-Type':'Application/json',
-          'Authorization':`Bearer ${cookie.token}`
-        }
-      }).then(async response=>{
-        const data = await response.json()
-        if(response.status === 200){
-          setUser(data)
-        }
-      })
+  // useEffect(()=>{
+  //   const getUser = () =>{
+  //     fetch('http://127.0.0.1:3001/api/auth/me',{
+  //       method:'GET',
+  //       headers:{
+  //         'Content-Type':'Application/json',
+  //         'Authorization':`Bearer ${cookie.token}`
+  //       }
+  //     }).then(async response=>{
+  //       const data = await response.json()
+  //       if(response.status === 200){
+  //         setUser(data)
+  //       }
+  //     })
       
-    }
-    getUser()
-  },[user])
+  //   }
+  //   getUser()
+  // },[user])
 
   return (
     <>
     <BrowserRouter>
       <Routes>
         <Route path='/categorySelectionPage' element={<CategorySelectionPage/>}></Route>
-        <Route path='repoDetails' element={<RepoDetails/>}></Route>
+        <Route path='repoDetails/:id' element={<RepoDetails/>}></Route>
         <Route path='/githubRepos' element={<GitHubRepositories/>}></Route>
         <Route path='/home' element={<HomePage/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
